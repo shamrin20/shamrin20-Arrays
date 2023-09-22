061d852eae2c4704b0cbc572c480e5b06d859fe5
shamrin20 <shamrinmillet20@gmail.com>
2023-09-22 22:27:15 +0530

    complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,24 +1,12 @@
 
-/**
- * An array is a group of some predefined number of elements.
- * For instance, we could have an array of 10 ints or 10 Strings.
- *
- * Or, we could have an array of our own custom Objects, such as our own Bird class for a birdwatching
- * application. This would allow us to keep track of many birds and their own behavior.
- *
- * For the following problems, all of the syntax we need is in this documentation:
- * https://www.learnjavaonline.org/en/Arrays
- */
 public class Lab {
     /**
      * @param arr the array we're getting data from.
      * @return the length of arr.
      */
     public int getLengthOfArray(int[] arr){
-        return 0;
+        return arr.length; // Return the actual length of the array
     }
-
-
 
     /**
      * @param arr the array we're getting data from.
@@ -27,11 +15,14 @@ public class Lab {
      * @return the element at index n of arr.
      */
     public int getNthElementOfArray(int[] arr, int n){
-        return 0;
+        if (n >= 0 && n < arr.length) {
+            return arr[n]; // Return the element at index n
+        } else {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
     }
 
     /**
-     *
      * @param arr the array we're getting data from.
      * @param n the index (number) of the element we're looking to modify.
      *          Indexes in arrays start counting from zero.
@@ -39,7 +30,11 @@ public class Lab {
      * @return nothing. Because of pass-by-reference, any change to the array will be reflected across our java program.
      */
     public void setNthElementOfArray(int[] arr, int n, int val){
-
+        if (n >= 0 && n < arr.length) {
+            arr[n] = val; // Set the element at index n to the specified value
+        } else {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
     }
 
     /**
@@ -47,6 +42,6 @@ public class Lab {
      * @return a new int array that can contain n elements.
      */
     public int[] returnNewArraySizeN(int n){
-        return null;
+        return new int[n]; // Create a new int array of size n
     }
 }
